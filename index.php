dcc7af9c4238665fecfc89a220b554209689e955
Prateek <prateekroopra@gmail.com>
2019-04-22 20:19:19 +0530

    Header Updates

--- index.php
+++ index.php
@@ -137,7 +137,7 @@
             <div class="col-sm-6 site-block img-mt-40" data-toggle="animation-appear" data-animation-class="animation-fadeInRight" data-element-offset="-180">
                 <img src="/img/FIND.FILE.FUND.png" alt="How it Works" class="img-responsive rounded-corners">
             </div>
-            <div class="col-sm-6 col-md-5 col-md-offset-1 site-block mt-neg50 m-mt-none" data-toggle="animation-appear" data-animation-class="animation-fadeInLeft" data-element-offset="-180">
+            <div class="col-sm-6 col-md-5 col-md-offset-1 site-block m-mt-none" data-toggle="animation-appear" data-animation-class="animation-fadeInLeft" data-element-offset="-180">
                 <h3 class="h2 site-heading site-heading-promo">
                     <span class="text-theme-dark-navy">
                         <strong>How It Works</strong>
@@ -300,134 +300,139 @@
 <script src="js/pages/landing.js"></script>
 <?php include 'inc/template_end.php'; ?>
 <script>
-    var container = document.getElementById('header-animation-inner')
-                    
-                    function resize() {
-                        container.style.fontSize = Math.round(container.offsetHeight * 0.11112) + 'px'
-                        container.style.lineHeight = Math.round(container.offsetHeight * 0.11112) + 'px'
-                    }
-            
-                    window.addEventListener('resize', resize, false)
-                    resize()
-            
-                    var spring = 'spring(0.5, 100, 10, 0)'
-                    var spring2 = 'spring(0.15, 100, 20, 0)'
-            
-                    var timeline = anime.timeline({ autoplay: false })
-            
-                    var group1 = {
-                        targets: '#header-text-group-1',
-                        top: [{ value: ['-100%', 0], duration: 1000 }],
-                        left: [
-                            { value: 0, duration: 5500, easing: 'linear' },
-                            { value: '-100%', duration: 1000 },
-                        ],
-                        opacity: [
-                            { value: [0, 1], duration: 1000 },
-                            { value: 1, duration: 5000, easing: 'linear' },
-                            { value: [1, 0], duration: 500 }
-                        ]
-                    }
-            
-                    var action = {
-                        targets: '#header-text-action',
-                        scaleY: [
-                            { value: [0, 1], duration: 800 },
-                            { value: 1, duration: 5000, easing: 'linear' },
-                            { value: [1, 0], duration: 800 },
-                        ],
-                        left: [
-                            { value: 0, duration: 5000, easing: 'linear' },
-                            { value: '-100%', duration: 1000 },
-                        ]
-                    }
-            
-                    var keeps = {
-                        targets: '#header-text-keeps',
-                        left: [
-                            { value: ['150%', '0%'], duration: 500, easing: spring },
-                            { value: 0, duration: 2000, easing: 'linear' },
-                            { value: ['0%', '150%'], duration: 500, easing: spring }
-                        ],
-                        opacity: [
-                            { value: [0, 1], duration: 200, easing: spring },
-                            { value: [1, 0.9], duration: 2300, easing: 'linear' },
-                            { value: [0.8, 0], duration: 100, easing: 'linear' },
-                        ]
-                    }
-            
-                    var hundreds = {
-                        targets: '#header-text-hundreds',
-                        left: [
-                            { value: ['150%', '0%'], duration: 500, easing: spring },
-                            { value: 0, duration: 2000, easing: 'linear' },
-                            { value: ['0%', '150%'], duration: 500, easing: spring }
-                        ],
-                        opacity: [
-                            { value: [0, 1], duration: 200, easing: spring },
-                            { value: [1, 0.9], duration: 2300, easing: 'linear' },
-                            { value: [0.8, 0], duration: 100, easing: 'linear' },
-                        ],
-                        scaleY: [
-                            { value: [0, 1], duration: 500, easing: spring },
-                            { value: 1, duration: 2000, easing: 'linear' },
-                            { value: [1, 0], duration: 2000, easing: spring }
-                        ]
-                    }
-            
-                    var and = {
-                        targets: '#header-text-and',
-                        left: [
-                            { value: ['150%', '0%'], duration: 500, easing: spring },
-                            { value: 0, duration: 2000, easing: 'linear' },
-                            { value: ['0%', '150%'], duration: 500, easing: spring }
-                        ],
-                        opacity: [
-                            { value: [0, 1], duration: 200, easing: spring },
-                            { value: [1, 0.9], duration: 2300, easing: 'linear' },
-                            { value: [0.8, 0], duration: 100, easing: 'linear' },
-                        ]
-                    }
-            
-                    var helps = {
-                        targets: '#header-text-helps',
-                        top: [-500, 0],
-                        opacity: [0, 1],
-                        duration: 500,
-                        easing: spring
-                    }
-            
-                    var svg = {
-                        targets: '#clarence-header-svg-container',
-                        top: [500, 0],
-                        opacity: [0, 1],
-                        duration: 500,
-                        easing: spring
-                    }
-            
-                    var clarence = {
-                        targets: '#clarence-header-svg-container svg',
-                        top: [500, '12%'],
-                        opacity: [0, 1],
-                        duration: 500,
-                        easing: spring2
-                    }
-            
-                    timeline
-                        .add(group1, 100)
-                        .add(action, 350)
-                        .add(Object.assign(action, { targets: '#header-text-inc' }), 450)
-                        .add(keeps, 600)
-                        .add(hundreds, 700)
-                        .add(Object.assign(hundreds, { targets: '#header-text-settlements' }), 750)
-                        .add(Object.assign(keeps, { targets: '#header-text-and' }), 3000)
-                        .add(helps, 3300)
-                        .add(Object.assign(hundreds, { targets: '#header-text-determine' }), 3100)
-                        .add(Object.assign(hundreds, { targets: '#header-text-eligibility' }), 3150)
-                        .add(svg, 5900)
-                        .add(clarence, 6000)
-                        .add(Object.assign(svg, { targets: '#header-text-meet-clarence' }), 6100)
-                        .add(Object.assign(svg, { targets: '#header-text-clarence' }), 6200)
-                        .add(Object.assign(svg, { targets: '#header-text-start-button' }), 6300)
-                        .play()
+      var container = document.getElementById('header-animation-inner')
+
+function resize() {
+    container.style.fontSize = Math.round(container.offsetHeight * 0.11112) + 'px'
+    container.style.lineHeight = Math.round(container.offsetHeight * 0.11112) + 'px'
+}
+
+window.addEventListener('resize', resize, false)
+resize()
+
+var spring = 'spring(0.5, 100, 10, 0)'
+var spring2 = 'spring(0.15, 100, 20, 0)'
+
+var timeline = anime.timeline({ autoplay: false })
+
+var group1 = {
+    targets: '#header-text-group-1',
+    top: [{ value: ['-100%', 0], duration: 1000 }],
+    left: [
+        { value: 0, duration: 5500, easing: 'linear' },
+        { value: '-100%', duration: 1000 },
+    ],
+    opacity: [
+        { value: [0, 1], duration: 1000 },
+        { value: 1, duration: 5000, easing: 'linear' },
+        { value: [1, 0], duration: 500 }
+    ]
+}
+
+var action = {
+    targets: '#header-text-action',
+    scaleY: [
+        { value: [0, 1], duration: 800 },
+        { value: 1, duration: 5000, easing: 'linear' },
+        { value: [1, 0], duration: 800 },
+    ],
+    left: [
+        { value: 0, duration: 5000, easing: 'linear' },
+        { value: '-100%', duration: 1000 },
+    ]
+}
+
+var keeps = {
+    targets: '#header-text-keeps',
+    left: [
+        { value: ['150%', '0%'], duration: 500, easing: spring },
+        { value: 0, duration: 2000, easing: 'linear' },
+        { value: ['0%', '150%'], duration: 500, easing: spring }
+    ],
+    opacity: [
+        { value: [0, 1], duration: 200, easing: spring },
+        { value: [1, 0.9], duration: 2300, easing: 'linear' },
+        { value: [0.8, 0], duration: 100, easing: 'linear' },
+    ]
+}
+
+var hundreds = {
+    targets: '#header-text-hundreds',
+    left: [
+        { value: ['150%', '0%'], duration: 500, easing: spring },
+        { value: 0, duration: 2000, easing: 'linear' },
+        { value: ['0%', '150%'], duration: 500, easing: spring }
+    ],
+    opacity: [
+        { value: [0, 1], duration: 200, easing: spring },
+        { value: [1, 0.9], duration: 2300, easing: 'linear' },
+        { value: [0.8, 0], duration: 100, easing: 'linear' },
+    ],
+    scaleY: [
+        { value: [0, 1], duration: 500, easing: spring },
+        { value: 1, duration: 2000, easing: 'linear' },
+        { value: [1, 0], duration: 2000, easing: spring }
+    ]
+}
+
+var and = {
+    targets: '#header-text-and',
+    left: [
+        { value: ['150%', '0%'], duration: 500, easing: spring },
+        { value: 0, duration: 2000, easing: 'linear' },
+        { value: ['0%', '150%'], duration: 500, easing: spring }
+    ],
+    opacity: [
+        { value: [0, 1], duration: 200, easing: spring },
+        { value: [1, 0.9], duration: 2300, easing: 'linear' },
+        { value: [0.8, 0], duration: 100, easing: 'linear' },
+    ]
+}
+
+var helps = {
+    targets: '#header-text-helps',
+    top: [-500, 0],
+    opacity: [0, 1],
+    duration: 500,
+    easing: spring
+}
+
+var svg = {
+    targets: '#clarence-header-svg-container',
+    top: [500, 0],
+    opacity: [0, 1],
+    duration: 500,
+    easing: spring
+}
+
+var clarence = {
+    targets: '#clarence-header-svg-container svg',
+    top: [500, '12%'],
+    opacity: [0, 1],
+    duration: 500,
+    easing: spring2
+}
+
+timeline
+    .add(group1, 100)
+    .add(action, 350)
+    .add(Object.assign(action, { targets: '#header-text-inc' }), 450)
+    .add(keeps, 600)
+    .add(hundreds, 700)
+    .add(Object.assign(hundreds, { targets: '#header-text-settlements' }), 750)
+    .add(Object.assign(keeps, { targets: '#header-text-and' }), 3000)
+    .add(helps, 3300)
+    .add(Object.assign(hundreds, { targets: '#header-text-determine' }), 3100)
+    .add(Object.assign(hundreds, { targets: '#header-text-eligibility' }), 3150)
+    .add(svg, 5900)
+    .add(clarence, 6000)
+    .add(Object.assign(svg, { targets: '#header-text-meet-clarence' }), 6100)
+    .add(Object.assign(svg, { targets: '#header-text-clarence' }), 6200)
+    .add(Object.assign(svg, { 
+        targets: '#header-text-start-button',
+        complete: function(){
+            document.getElementById('header-text-start-button').style.pointerEvents = 'all'
+        }
+    }), 6300)
+    .play()
 </script>
